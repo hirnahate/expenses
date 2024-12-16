7386e0efdbb0c0b5a6b6c5da9c97c2ac9cb44178
Hirna Derege <168405714+hirnahate@users.noreply.github.com>
2024-12-16 00:22:51 -0800

    Initial Commit

---
+++ expenses/expensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  expensesApp.swift
+//  expenses
+//
+//  Created by Hirna Derege on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct expensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ expensesTests/expensesTests.swift
@@ -0,0 +1,16 @@
+//
+//  expensesTests.swift
+//  expensesTests
+//
+//  Created by Hirna Derege on 12/16/24.
+//
+
+import Testing
+
+struct expensesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
